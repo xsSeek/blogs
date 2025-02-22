432e9466d5d1828b49fa55e23805f850fe33148e
xi <xo@outlook.com>
2025-02-22 16:07:49 +0800

    .

--- docs/.vuepress/conf/sideBarConf.js
+++ docs/.vuepress/conf/sideBarConf.js
@@ -1,15 +1,16 @@
 module.exports = {
-  '/': [
-    ['2.js', 'ECMAScript'],
-    ['6.css', 'CSS3'],
-    ['a.htm', 'html5'],
-    ['8.browser', '浏览器'],
-    ['9.http', 'HTTP协议'],
-    ['3.0.vue', 'VUE'],
-    ['4.react', 'REACT'],
-    ['5.webpack', 'WEBPACK'],
-    ['7.arithmetic', '数据结构和算法'],
-    ['10.utils', '工具函数'],
-    ['11.lodash', 'Lodash'],
-  ]
+    '/': [
+        ['6.css', 'CSS3'],
+        ['a.htm', 'html5'],
+        ['8.browser', '浏览器'],
+        ['9.http', 'HTTP协议'],
+        ['2.js', 'ECMAScript'],
+        ['2.1.array', '数组'],
+        ['3.0.vue', 'VUE'],
+        ['4.react', 'REACT'],
+        ['5.webpack', 'WEBPACK'],
+        ['7.arithmetic', '数据结构和算法'],
+        ['10.utils', '工具函数'],
+        ['11.lodash', 'Lodash'],
+    ]
 }
